7f6aa0baf0ab2daf84ada0b8825976c57063e365
Hosh <domingohoshaiah@gmail.com>
2023-09-24 12:29:51 +0800

    permitted adjacencyList and nodeOccupancy as parameters

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,28 +1,28 @@
 class GraphsController < ApplicationController
     def index
-      @graphs = Graph.all
+        @graphs = Graph.all
     end
   
     def new
-      @graph = Graph.new
+        @graph = Graph.new
     end
   
     def create
-      @graph = Graph.new(graph_params)
-      if @graph.save
-        render json: @graph
-      else
-        render 'new'
-      end
+        @graph = Graph.new(adjacencyList: graph_params['adjacencyList'], nodeOccupancy: graph_params['nodeOccupancy'])
+        if @graph.save
+            render json: @graph
+        else
+            render 'new'
+        end
     end
   
     def show
-      @graph = Graph.find(params[:id])
+        @graph = Graph.find(params[:id])
     end
   
     private
   
     def graph_params
-      params.require(:graph).permit(:adjacencyList, :nodeOccupancy)
+        params.require(:graph).permit(adjacencyList: {}, nodeOccupancy: {})
     end
 end
